70d6180139a184869f533175c55b9c08d5e68c05
OlehZin <biosoln91@gmail.com>
2020-06-03 23:44:03 +0300

    second commit

--- opendota.rb
+++ opendota.rb
@@ -1,30 +1,36 @@
 require 'httparty'
-require 'open-uri'
+require 'pry'
 
 class ODApi
+
+  include HTTParty
+  base_uri 'https://www.opendota.com'
+
+
+  def heroes
+    self.class.get("/api/heroes")
+  end
+
+
+  def matches(hero_id)
+    @responce = self.class.get("/api/heroes/#{hero_id}/matches") 
+    check(@responce)
+  end
     
-    include HTTParty
-    base_uri 'https://www.opendota.com'
-
-
-    def heroes
-        self.class.get("/api/heroes")
+  def check(responce)
+    if @responce.body == "[]"
+      'Exeption! Current id does not exist'
+    else 
+      responce.body
     end
+  end
 
-
-    def matches(hero_id)
-        begin
-            open("https://api.opendota.com/api/heroes/#{hero_id}/matches") {|f| puts f.readlines }
-        rescue OpenURI::HTTPError => ex
-            puts ("#{hero_id} id doesn't exist!")
-        end
-    end
- 
-    
 end
 
 
 opendota = ODApi.new
 puts opendota.heroes
 puts "--------------------------------------"
-puts opendota.matches(2000)
+puts opendota.matches(100)
+puts "--------------------------------------"
+puts opendota.matches(999)
